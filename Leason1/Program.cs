5ef56be05ca827ac6fda2e6487749afd54cd0ebf
Konstantin <kostik-kostik-12@mail.ru>
2023-05-25 22:31:03 +0300

    add Leason

---
+++ Leason1/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Задача 39: Напишите программу, которая перевернёт одномерный 
+// массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+//Метод1:
+
+Console.Clear();
+int[] array = GetArray(9, 0, 10);
+Console.WriteLine(String.Join(", ", array));
+ReversArray1(array);
+Console.WriteLine(String.Join(", ", array));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue);
+    }
+    return res;
+}
+
+void ReversArray1(int[] inArray)
+{
+    for(int i = 0; i < inArray.Length/2; i++)
+    {
+        int temp = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length -1 - i] = temp;
+    }
+}
---
+++ Leason2/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Задача 40: Напишите программу, которая принимает на
+// вход три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+int Prompt()
+{
+    Console.Write("Введите сторону треугольника: ");
+    int side = int.Parse(Console.ReadLine());
+    return side;
+}
+
+void CheckValid(int sideA, int sideB, int sideC)
+{
+    bool check = false;
+    if (sideA<(sideB+sideC) && sideB<(sideA+sideC) && sideC<(sideB+sideA))  
+    {
+        check = true;
+    }
+    if(check == true)
+    { 
+    Console.WriteLine("Треугольник с заданными сторонами существует");
+    }
+    else
+    {
+    Console.WriteLine("Треугольник с заданными сторонами не существует");
+    }
+}
+
+int sideA = Prompt();
+int sideB = Prompt();
+int sideC = Prompt();
+
+CheckValid(sideA, sideB, sideC);
